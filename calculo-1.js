2cee68f7266fcf886e9afa271dccd5de71ce7a10
Brenno Farias <brenno.developer@gmail.com>
2022-09-17 19:37:42 -0300

    First commit

---
+++ calculo-1.js
@@ -0,0 +1,29 @@
+let param1 = calcMedia(100,100);
+let param2 = calcMedia(90,90);
+let mediaExp = calcMediaExp(param1,param2);
+
+function validaJogo (){
+
+}
+
+
+function calcMedia (a,b){
+    if(a,b<60){
+       return "Desaprovado por parâmetro abaixo da média";
+    }else{
+        return (a+b)/2;
+    }
+}
+
+function calcMediaExp (param1,param2){
+    if(param1,param2<60){
+        return "Desaprovado por parâmetro abaixo da média";
+     }else{
+         return (param1+param2)/2;
+     }
+}
+
+
+console.log ("Média casa e fora "+param1+"%");
+console.log ("Média geral "+param2+"%");
+console.log ("Média exponencial "+mediaExp+"%");
