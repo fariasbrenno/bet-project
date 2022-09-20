fa840681c2be66c7ece3288dd22bb2675601dfe9
Brenno Farias <brenno.developer@gmail.com>
2022-09-20 17:56:41 -0300

    Desenvolvendo código js

--- js/calculo-1.js
+++ js/calculo-1.js
@@ -1,38 +1,11 @@
-function validaJogo (){
-    var rodada = document.getElementById('rodada').value;
-    var casa = document.getElementById('vCasa').value; 
-    var fora = document.getElementById('vFora').value; 
-    var casaGeral = document.getElementById('casaG').value; 
-    var foraGeral = document.getElementById('foraG').value;
 
-    if(rodada < 12){
-        document.write("Rodada menor que a permitida");
-    }
-    
-}
-    /**
-    function calcMedia (a,b){
-        if(a,b<60){
-           return "Desaprovado por parâmetro abaixo da média";
-        }else{
-            return (a+b)/2;
-        }
-    }
-    function calcMediaExp (param1,param2){
-        if(param1,param2<60){
-            return "Desaprovado por parâmetro abaixo da média";
-         }else{
-             return (param1+param2)/2;
-         }
-    }
-    
-    let param1 = calcMedia(100,100);
-    let param2 = calcMedia(90,90);
-    let mediaExp = calcMediaExp(param1,param2);
-    
-    console.log ("Média casa e fora "+param1+"%");
-    console.log ("Média geral "+param2+"%");
-    console.log ("Média exponencial "+mediaExp+"%");
+function calcCasaFora (vCasa, vFora){
 
+    if(vCasa, vFora<60){
+        return "Desaprovado por parâmetro abaixo da média";
+    }else{
+        return (vCasa+vFora)/2;
+    }
 
-console.log (validaJogo());**/
+} 
+document.getElementById("texto").innerHTML = calcCasaFora(80,80)      
