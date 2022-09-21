6a6da4d00f2886acbec6d7c9ffb352cf9064cce8
Brenno Farias <brenno.developer@gmail.com>
2022-09-20 21:12:00 -0300

    cálculos com javascript

--- js/calculo-1.js
+++ js/calculo-1.js
@@ -1,11 +1,32 @@
 
-function calcCasaFora (vCasa, vFora){
 
-    if(vCasa, vFora<60){
-        return "Desaprovado por parâmetro abaixo da média";
-    }else{
-        return (vCasa+vFora)/2;
+function calcCasaFora (){ 
+    var vCasa = document.getElementById("vCasa").value;
+    var vFora = document.getElementById("vFora").value;
+    var rodada = document.getElementById("rodada").value;
+    var casaG = document.getElementById("casaG").value;
+    var foraG = document.getElementById("foraG").value;
+
+    function calculos (){
+        if(rodada<12){
+            return document.getElementById("texto").innerHTML = "<span style='color: red;'>Não possui o número de rodadas suficientes</span>";
+        }
+        if((vCasa < 60) || (vFora < 60)){
+            return document.getElementById("texto").innerHTML = "<span style='color:red;'> Sem critério % casa ou fora </span>";
+        }else{
+            var calcCasaFora = (parseFloat(vCasa)+parseFloat(vFora))/2;
+            // document.getElementById("texto").innerHTML = calcCasaFora;
+        }   
+        if((casaG<60) || (foraG<60)){
+            return document.getElementById("texto").innerHTML = "<span style='color:red;'> Sem critério GERAL </span>";
+        }else{
+            var calcGeral = (parseFloat(casaG)+parseFloat(foraG))/2;
+            // document.getElementById("texto").innerHTML = calcGeral
+        } 
+        return document.getElementById("texto").innerHTML = "<span style='color:green;'> APROVADO com " + ((calcCasaFora+calcGeral)/2) + "%</span>";
     }
 
-} 
-document.getElementById("texto").innerHTML = calcCasaFora(80,80)      
+    return calculos();
+}
+
+
