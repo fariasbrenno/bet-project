90af9fcef0d46156befc29ce674637780b2bb4eb
Brenno Farias <38961716+fariasbrenno@users.noreply.github.com>
2022-11-02 21:32:18 -0300

    Alterando algoritmo para funções com passagem de parametros

--- js/calculo-1.js
+++ js/calculo-1.js
@@ -28,10 +28,32 @@ function calcCasaFora (){
 
     return calculos();*/
     function calculation (rodada, casa, fora, casaGeral, foraGeral) {
-          
+        
+        if(rodada < 12){
+            return document.getElementById("texto").innerHTML = "<span style='color: red;'>Não possui o número de rodadas suficientes</span>";
+        }
+        if((casa < 60) || (fora < 60)){
+            return document.getElementById("texto").innerHTML = "<span style='color:red;'> Sem critério % casa ou fora </span>";
+        }else{
+            var calcCasaFora = (parseFloat(casa)+parseFloat(fora))/2;
+            // document.getElementById("texto").innerHTML = calcCasaFora;
+        }   
+        if((casaGeral<60) || (foraGeral<60)){
+            return document.getElementById("texto").innerHTML = "<span style='color:red;'> Sem critério GERAL </span>";
+        }else{
+            var calcGeral = (parseFloat(casaG)+parseFloat(foraG))/2;
+            // document.getElementById("texto").innerHTML = calcGeral
+        } 
+        return document.getElementById("texto").innerHTML = "<span style='color:green;'> APROVADO com " + ((calcCasaFora+calcGeral)/2) + "%</span>";
     }
 
     let rod = document.getElementById("rodada").value;
+    let vCasa = document.getElementById("vCasa").value;
+    let vFora = document.getElementById("vFora").value;
+    let casaG = document.getElementById("casaG").value;
+    let foraG = document.getElementById("foraG").value;
+
+    calculation(rod, vCasa, vFora, casaG, foraG);
 
     
 }
